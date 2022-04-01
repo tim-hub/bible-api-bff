31b95221763e1f6b8e81860e9df3bb32af808400
tim-hub <tim-hub@users.noreply.github.com>
2022-04-02 12:01:01 +1300

    add ts-node-dev for watch

---
+++ README.md
@@ -0,0 +1,4 @@
+## Bible API BFF (Back-end for Front-end)
+
+
+
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,12 @@
 import 'dotenv/config';
 import * as express from 'express';
+const port = process.env.PORT ? process.env.PORT: 3000;
 const app = express();
 
 app.get('/', (req, res) => {
     return res.send('Received a GET HTTP method');
 });
-app.post('/', (req, res) => {
-    return res.send('Received a POST HTTP method');
-});
-app.put('/', (req, res) => {
-    return res.send('Received a PUT HTTP method');
-});
-app.delete('/', (req, res) => {
-    return res.send('Received a DELETE HTTP method');
-});
-app.listen(process.env.PORT, () =>
-    console.log(` -- Example app listening on port ${process.env.PORT}!`),
+
+app.listen( port , () =>
+    console.log(`listening on port ${port}`),
 );
