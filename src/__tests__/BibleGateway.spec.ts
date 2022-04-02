932299f183f78ec493e8d5fa11d8e340b5f16491
tim-hub <tim-hub@users.noreply.github.com>
2022-04-02 17:12:19 +1300

    remove bible gateway

--- src/__tests__/BibleGateway.spec.ts
+++
@@ -1,8 +0,0 @@
-import {BibleGatewayAPI} from '../provider/BibleGateway';
-
-// test BibleGatewayAPI through jest
-describe('BibleGatewayAPI', () => {
-  it('should be defined', () => {
-    expect(new BibleGatewayAPI()).toBeDefined();
-  });
-});
